31926aa35b82424cacb1688cdf98ef42fb3e05ab
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-02-19 13:06:04 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+
+import { useState } from 'react';
+import './App.css';
+import './Component/Calculator'
+import Project from '../src/img/Muhammadislom.jpg'
+
+
+
+function App() {
+  const [modal, setModal] = useState(false)
+  
+  const openModal = (e) =>{
+    e.preventDefault()
+    setModal(!modal)
+  }
+
+  const abdulla = (event) => {
+    event.preventDefault()
+    // const {name} = event.target;
+    
+    setModal(!modal)
+
+    event.target[0].value =null;
+    event.target[1].value =null;
+
+      console.log(event.target[0].value)
+  }
+  
+  return (
+    <div className="App">
+       <div className="page">
+       <button className='button' onClick={openModal}>Modal Open</button>
+       </div>
+
+       <dialog className='dialog' open={modal}>
+         <form className='form' onSubmit={abdulla}>
+       <div className="form-floating">
+            <input type="email" name='email'  class="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+            <input type="password"name='password' class="form-controll"  id="floatingPassword" placeholder="Password" />
+            <label for="floatingPassword">Password</label>
+        </div>
+        <button className="btn">
+          Submit
+        </button>
+        </form>
+       </dialog>
+       <div className="box">
+         <img className='pictures' src={Project} alt="" />
+       </div>
+    </div>
+  );
+}
+
+export default App;
